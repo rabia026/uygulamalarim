e16e7214a6decaffd31510a616945944e749ed05
rabia026 <34534193+rabia026@users.noreply.github.com>
2017-12-16 15:05:11 +0300

    uygulama 11

---
+++ karakter sayısı bul.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+    char ad[20];
+    cout<<"isminizi girin:";
+    cin>>ad;
+    cout<<" karakter uzunluk:"<<strlen(ad);
+    return 0;
+}
+
