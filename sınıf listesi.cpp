94c5ebc5f56d9d0570231b03e11eb60401f33b50
rabia026 <34534193+rabia026@users.noreply.github.com>
2017-12-23 14:22:27 +0300

    sınıf listesi

---
+++ sınıf listesi.cpp
@@ -0,0 +1,186 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    system ("COLOR A0");
+    getch ();
+    cout << "Sinif Listesi ve Ders Ortalamalari" << endl;
+    cout << "9. Siniflardan gormek istediginiz subeyi buyuk harfle giriniz." << endl;
+    char a;
+    cin >> a;
+    switch (a) {
+    case 'A':
+        system ("COLOR 37");
+        getch ();
+        cout << "9/A SINIFI" << endl;
+        cout << "1. Ayse CIHAN     NUMARASI: 20       ORT: 96,24" <<  endl;
+        cout << "2. Nur MELEK      NUMARASI: 42       ORT: 85,30" << endl;
+        cout << "3. Ela CETIN      NUMARASI: 55       ORT: 86,50" << endl;
+        cout << "4. Ismail BAK     NUMARASI: 60       ORT: 70,62" << endl;
+        cout << "5. Tamer ER       NUMARASI: 63       ORT: 77,60" << endl;
+        cout << "6. Firat SALIH    NUMARASI: 69       ORT: 85,60" << endl;
+        cout << "7. Almila AHENK   NUMARASI: 71       ORT: 99,50" << endl;
+        cout << "8. Alican AHI     NUMARASI: 73       ORT: 65,32" << endl;
+        cout << "9. Ata ASA        NUMARASI: 75       ORT: 63,20" << endl;
+        cout << "10. Melek ATALAY  NUMARASI: 85       ORT: 96,72" << endl;
+        cout << "SINIF ORTALAMASI: 82,66" << endl;
+        system ("COLOR DF");
+        getch ();
+        break;
+    case 'B':
+        system ("COLOR 90");
+        getch ();
+        cout << "9/B SINIFI" << endl;
+        cout << "1. Arda AYTEKIN   NUMARASI: 10       ORT: 55,30" << endl;
+        cout << "2. Bahadir YIGIT  NUMARASI: 15       ORT: 77,62" << endl;
+        cout << "3. Behlul GUCLU   NUMARASI: 17       ORT: 90,30" << endl;
+        cout << "4. Bera SU        NUMARASI: 18       ORT: 99,63" << endl;
+        cout << "5. Zeynep KAR     NUMARASI: 19       ORT: 95,42" << endl;
+        cout << "6. Irem ARKA      NUMARASI: 22       ORT: 85,88" << endl;
+        cout << "7. Acelya AT      NUMARASI: 25       ORT: 96,72" << endl;
+        cout << "8. Badem GUL      NUMARASI: 28       ORT: 75,84" << endl;
+        cout << "9. Didem DENIZ    NUMARASI: 29       ORT: 95,21" << endl;
+        cout << "10. Derya YAR     NUMARASI: 32       ORT: 98,55" << endl;
+        cout << "SINIF ORTALAMASI: 87,047" << endl;
+        system ("COLOR 47");
+        getch ();
+    case 'C':
+        system ("COLOR 0E");
+        getch ();
+        cout << "9/C SINIFI" << endl;
+        cout << "1. Duygu DUS      NUMARASI: 33       ORT: 88,66" << endl;
+        cout << "2. Orhan INCE     NUMARASI: 34       ORT: 89,37" << endl;
+        cout << "3. Dolunay OT     NUMARASI: 35       ORT: 92,74" << endl;
+        cout << "4. Derin RAY      NUMARASI: 37       ORT: 94,63" << endl;
+        cout << "5. Zeyno SAR      NUMARASI: 39       ORT: 95,72" << endl;
+        cout << "6. Kader ANKA     NUMARASI: 40       ORT: 81,88" << endl;
+        cout << "7. Arda BADEM     NUMARASI: 41       ORT: 94,73" << endl;
+        cout << "8. Adnan AK       NUMARASI: 43       ORT: 75,84" << endl;
+        cout << "9. Emre CICEK     NUMARASI: 44       ORT: 45,21" << endl;
+        cout << "10. Bilal ILK     NUMARASI: 46       ORT: 98,55" << endl;
+        cout << "SINIF ORTALAMASI: 85,733" << endl;
+        system ("COLOR B0");
+        getch ();
+    }
+    char x;
+    cin >> x;
+    switch (x) {
+    case 'A':
+        system ("COLOR 37");
+        getch ();
+        cout << "9/A SINIFI" << endl;
+        cout << "1. Ayse CIHAN     NUMARASI: 20       ORT: 96,24" <<  endl;
+        cout << "2. Nur MELEK      NUMARASI: 42       ORT: 85,30" << endl;
+        cout << "3. Ela CETIN      NUMARASI: 55       ORT: 86,50" << endl;
+        cout << "4. Ismail BAK     NUMARASI: 60       ORT: 70,62" << endl;
+        cout << "5. Tamer ER       NUMARASI: 63       ORT: 77,60" << endl;
+        cout << "6. Firat SALIH    NUMARASI: 69       ORT: 85,60" << endl;
+        cout << "7. Almila AHENK   NUMARASI: 71       ORT: 99,50" << endl;
+        cout << "8. Alican AHI     NUMARASI: 73       ORT: 65,32" << endl;
+        cout << "9. Ata ASA        NUMARASI: 75       ORT: 63,20" << endl;
+        cout << "10. Melek ATALAY  NUMARASI: 85       ORT: 96,72" << endl;
+        cout << "SINIF ORTALAMASI: 82,66" << endl;
+        system ("COLOR DF");
+        getch ();
+        break;
+    case 'B':
+        system ("COLOR 90");
+        getch ();
+        cout << "9/B SINIFI" << endl;
+        cout << "1. Arda AYTEKIN   NUMARASI: 10       ORT: 55,30" << endl;
+        cout << "2. Bahadir YIGIT  NUMARASI: 15       ORT: 77,62" << endl;
+        cout << "3. Behlul GUCLU   NUMARASI: 17       ORT: 90,30" << endl;
+        cout << "4. Bera SU        NUMARASI: 18       ORT: 99,63" << endl;
+        cout << "5. Zeynep KAR     NUMARASI: 19       ORT: 95,42" << endl;
+        cout << "6. Irem ARKA      NUMARASI: 22       ORT: 85,88" << endl;
+        cout << "7. Acelya AT      NUMARASI: 25       ORT: 96,72" << endl;
+        cout << "8. Badem GUL      NUMARASI: 28       ORT: 75,84" << endl;
+        cout << "9. Didem DENIZ    NUMARASI: 29       ORT: 95,21" << endl;
+        cout << "10. Derya YAR     NUMARASI: 32       ORT: 98,55" << endl;
+        cout << "SINIF ORTALAMASI: 87,047" << endl;
+        system ("COLOR 47");
+        getch ();
+    case 'C':
+        system ("COLOR 0E");
+        getch ();
+        cout << "9/C SINIFI" << endl;
+        cout << "1. Duygu DUS      NUMARASI: 33       ORT: 88,66" << endl;
+        cout << "2. Orhan INCE     NUMARASI: 34       ORT: 89,37" << endl;
+        cout << "3. Dolunay OT     NUMARASI: 35       ORT: 92,74" << endl;
+        cout << "4. Derin RAY      NUMARASI: 37       ORT: 94,63" << endl;
+        cout << "5. Zeyno SAR      NUMARASI: 39       ORT: 95,72" << endl;
+        cout << "6. Kader ANKA     NUMARASI: 40       ORT: 81,88" << endl;
+        cout << "7. Arda BADEM     NUMARASI: 41       ORT: 94,73" << endl;
+        cout << "8. Adnan AK       NUMARASI: 43       ORT: 75,84" << endl;
+        cout << "9. Emre CICEK     NUMARASI: 44       ORT: 45,21" << endl;
+        cout << "10. Bilal ILK     NUMARASI: 46       ORT: 98,55" << endl;
+        cout << "SINIF ORTALAMASI: 85,733" << endl;
+        system ("COLOR B0");
+        getch ();
+    }
+    char y;
+    cin >> y;
+    switch (y) {
+    case 'A':
+        system ("COLOR 37");
+        getch ();
+        cout << "9/A SINIFI" << endl;
+        cout << "1. Ayse CIHAN     NUMARASI: 20       ORT: 96,24" <<  endl;
+        cout << "2. Nur MELEK      NUMARASI: 42       ORT: 85,30" << endl;
+        cout << "3. Ela CETIN      NUMARASI: 55       ORT: 86,50" << endl;
+        cout << "4. Ismail BAK     NUMARASI: 60       ORT: 70,62" << endl;
+        cout << "5. Tamer ER       NUMARASI: 63       ORT: 77,60" << endl;
+        cout << "6. Firat SALIH    NUMARASI: 69       ORT: 85,60" << endl;
+        cout << "7. Almila AHENK   NUMARASI: 71       ORT: 99,50" << endl;
+        cout << "8. Alican AHI     NUMARASI: 73       ORT: 65,32" << endl;
+        cout << "9. Ata ASA        NUMARASI: 75       ORT: 63,20" << endl;
+        cout << "10. Melek ATALAY  NUMARASI: 85       ORT: 96,72" << endl;
+        cout << "SINIF ORTALAMASI: 82,66" << endl;
+        system ("COLOR DF");
+        getch ();
+        break;
+    case 'B':
+        system ("COLOR 90");
+        getch ();
+        cout << "9/B SINIFI" << endl;
+        cout << "1. Arda AYTEKIN   NUMARASI: 10       ORT: 55,30" << endl;
+        cout << "2. Bahadir YIGIT  NUMARASI: 15       ORT: 77,62" << endl;
+        cout << "3. Behlul GUCLU   NUMARASI: 17       ORT: 90,30" << endl;
+        cout << "4. Bera SU        NUMARASI: 18       ORT: 99,63" << endl;
+        cout << "5. Zeynep KAR     NUMARASI: 19       ORT: 95,42" << endl;
+        cout << "6. Irem ARKA      NUMARASI: 22       ORT: 85,88" << endl;
+        cout << "7. Acelya AT      NUMARASI: 25       ORT: 96,72" << endl;
+        cout << "8. Badem GUL      NUMARASI: 28       ORT: 75,84" << endl;
+        cout << "9. Didem DENIZ    NUMARASI: 29       ORT: 95,21" << endl;
+        cout << "10. Derya YAR     NUMARASI: 32       ORT: 98,55" << endl;
+        cout << "SINIF ORTALAMASI: 87,047" << endl;
+        system ("COLOR 47");
+        getch ();
+    case 'C':
+        system ("COLOR 0E");
+        getch ();
+        cout << "9/C SINIFI" << endl;
+        cout << "1. Duygu DUS      NUMARASI: 33       ORT: 88,66" << endl;
+        cout << "2. Orhan INCE     NUMARASI: 34       ORT: 89,37" << endl;
+        cout << "3. Dolunay OT     NUMARASI: 35       ORT: 92,74" << endl;
+        cout << "4. Derin RAY      NUMARASI: 37       ORT: 94,63" << endl;
+        cout << "5. Zeyno SAR      NUMARASI: 39       ORT: 95,72" << endl;
+        cout << "6. Kader ANKA     NUMARASI: 40       ORT: 81,88" << endl;
+        cout << "7. Arda BADEM     NUMARASI: 41       ORT: 94,73" << endl;
+        cout << "8. Adnan AK       NUMARASI: 43       ORT: 75,84" << endl;
+        cout << "9. Emre CICEK     NUMARASI: 44       ORT: 45,21" << endl;
+        cout << "10. Bilal ILK     NUMARASI: 46       ORT: 98,55" << endl;
+        cout << "SINIF ORTALAMASI: 85,733" << endl;
+        system ("COLOR B0");
+        getch ();
+    }
+    cout << "9. SINIFLARDAN; " << endl;
+    cout << "1. OLAN SUBE; 9/B " << endl;
+    cout << "2. OLAN SUBE; 9/C " << endl;
+    cout << "3. OLAN SUBE; 9/A " << endl;
+    return 0;
+}
