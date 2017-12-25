051c6cae313a88624d84afa6df8d2bcdf6ac2acb
rabia026 <34534193+rabia026@users.noreply.github.com>
2017-12-25 20:04:07 +0300

    uygula 1

---
+++ mainnn.cpp.cpp
@@ -0,0 +1,205 @@
+#include <iostream>
+#include <cstdlib>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    cout << "Hangi isareti gormek istiyorsunuz?" << endl;
+    cout << "A) SOL OK      B) SAG OK      C) ASAGI OK" << endl;
+    char a;
+    cin >> a;
+    switch (a) {
+    case 'A':
+        int x;
+         for (x = 1; x <= 2; x++) {
+                cout << "*************" << endl;
+        } for (x = 1; x <= 2; x++) {
+                cout << "              -----------------" << endl;
+        } for (x = 1; x <= 2; x++) {
+                cout << "                               ************************" << endl;
+        } for (x = 1; x <= 2; x++) {
+                cout << "                                                       -------------------->>>>>>>>" << endl;
+        } for (x = 1; x <= 2; x++) {
+                cout << "                               ************************" << endl;
+        } for (x = 1; x <= 2; x++) {
+                cout << "              -----------------" << endl;
+        } for (x = 1; x <= 2; x++) {
+                cout << "**************" << endl;
+        }
+        system ("COLOR 50");
+        getch ();
+    case 'B':
+        int y;
+        for (y = 1; y <= 2; y++ ) {
+            cout << "                                                           ***************************** " << endl;
+        }
+        for (y = 1; y <= 2; y++) {
+            cout << "                                   ------------------------" << endl;
+        }
+        for (y = 1; y <= 2; y++) {
+            cout << "                  ***************** " << endl;
+        }
+        for (y = 1; y <= 2; y++) {
+            cout << "<<<<--------------" << endl;
+        }
+        for (y = 1; y <= 2; y++) {
+            cout << "                  ***************** " << endl;
+        }
+        for (y = 1; y <= 2; y++) {
+            cout << "                                   ------------------------" << endl;
+        }
+        for (y = 1; y <= 2; y++) {
+            cout << "                                                           *****************************" << endl;
+        }
+        system ("COLOR 37");
+        getch ();
+    case 'C':
+        int z;
+        for (z = 1; z <= 10; z++) {
+            cout << "*                                     *" << endl;
+        }
+        for (z = 1; z <= 10; z++) {
+            cout << "         |                     |        " << endl;
+        }
+        for (z = 1; z <= 10; z++) {
+            cout << "                *       *" << endl;
+        }
+        for (z = 1; z <= 10; z++) {
+            cout << "                    |           " << endl;
+        }
+        system ("COLOR 9F");
+        getch ();
+        }
+    char b;
+    cin >> b;
+    switch (b) {
+    case 'A':
+        int x;
+         for (x = 1; x <= 2; x++) {
+                cout << "*************" << endl;
+        } for (x = 1; x <= 2; x++) {
+                cout << "              -----------------" << endl;
+        } for (x = 1; x <= 2; x++) {
+                cout << "                               ************************" << endl;
+        } for (x = 1; x <= 2; x++) {
+                cout << "                                                       -------------------->>>>>>>>" << endl;
+        } for (x = 1; x <= 2; x++) {
+                cout << "                               ************************" << endl;
+        } for (x = 1; x <= 2; x++) {
+                cout << "              -----------------" << endl;
+        } for (x = 1; x <= 2; x++) {
+                cout << "**************" << endl;
+        }
+        system ("COLOR 50");
+        getch ();
+    case 'B':
+        int y;
+        for (y = 1; y <= 2; y++ ) {
+            cout << "                                                           ***************************** " << endl;
+        }
+        for (y = 1; y <= 2; y++) {
+            cout << "                                   ------------------------" << endl;
+        }
+        for (y = 1; y <= 2; y++) {
+            cout << "                  ***************** " << endl;
+        }
+        for (y = 1; y <= 2; y++) {
+            cout << "<<<<--------------" << endl;
+        }
+        for (y = 1; y <= 2; y++) {
+            cout << "                  ***************** " << endl;
+        }
+        for (y = 1; y <= 2; y++) {
+            cout << "                                   ------------------------" << endl;
+        }
+        for (y = 1; y <= 2; y++) {
+            cout << "                                                           *****************************" << endl;
+        }
+        system ("COLOR 37");
+        getch ();
+    case 'C':
+        int z;
+        for (z = 1; z <= 10; z++) {
+            cout << "*                                     *" << endl;
+        }
+        for (z = 1; z <= 10; z++) {
+            cout << "         |                     |        " << endl;
+        }
+        for (z = 1; z <= 10; z++) {
+            cout << "                *       *" << endl;
+        }
+        for (z = 1; z <= 10; z++) {
+            cout << "                    |           " << endl;
+        }
+        system ("COLOR 9F");
+        getch ();
+        }
+    char c;
+    cin >> c;
+    switch (c) {
+    case 'A':
+        int x;
+         for (x = 1; x <= 2; x++) {
+                cout << "*************" << endl;
+        } for (x = 1; x <= 2; x++) {
+                cout << "              -----------------" << endl;
+        } for (x = 1; x <= 2; x++) {
+                cout << "                               ************************" << endl;
+        } for (x = 1; x <= 2; x++) {
+                cout << "                                                       -------------------->>>>>>>>" << endl;
+        } for (x = 1; x <= 2; x++) {
+                cout << "                               ************************" << endl;
+        } for (x = 1; x <= 2; x++) {
+                cout << "              -----------------" << endl;
+        } for (x = 1; x <= 2; x++) {
+                cout << "**************" << endl;
+        }
+        system ("COLOR 50");
+        getch ();
+    case 'B':
+        int y;
+        for (y = 1; y <= 2; y++ ) {
+            cout << "                                                           ***************************** " << endl;
+        }
+        for (y = 1; y <= 2; y++) {
+            cout << "                                   ------------------------" << endl;
+        }
+        for (y = 1; y <= 2; y++) {
+            cout << "                  ***************** " << endl;
+        }
+        for (y = 1; y <= 2; y++) {
+            cout << "<<<<--------------" << endl;
+        }
+        for (y = 1; y <= 2; y++) {
+            cout << "                  ***************** " << endl;
+        }
+        for (y = 1; y <= 2; y++) {
+            cout << "                                   ------------------------" << endl;
+        }
+        for (y = 1; y <= 2; y++) {
+            cout << "                                                           *****************************" << endl;
+        }
+        system ("COLOR 37");
+        getch ();
+    case 'C':
+        int z;
+        for (z = 1; z <= 10; z++) {
+            cout << "*                                     *" << endl;
+        }
+        for (z = 1; z <= 10; z++) {
+            cout << "         |                     |        " << endl;
+        }
+        for (z = 1; z <= 10; z++) {
+            cout << "                *       *" << endl;
+        }
+        for (z = 1; z <= 10; z++) {
+            cout << "                    |           " << endl;
+        }
+        system ("COLOR 9F");
+        getch ();
+        }
+
+    getch ();
+    return 0;
+}
